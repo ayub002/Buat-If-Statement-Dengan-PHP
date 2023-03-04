678129f4d81d1305ed28a176df5e16e02c735e8b
ayub002 <117551988+ayub002@users.noreply.github.com>
2023-03-05 07:09:49 +0900

    Add files via upload

---
+++ materi1/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pemograman PHP</title>
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="https://temorubun.site/ayub/php/">Pemograman <span class="badge bg-primary">PHP</span></a>
+      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-content">
+        <div class="hamburger-toggle">
+          <div class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </button>
+      <div class="collapse navbar-collapse" id="navbar-content">
+        <ul class="navbar-nav mr-auto mb-2 mb-lg-0 ">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" data-bs-auto-close="outside">TEORI PEMROGRAMAN</a>
+            <ul class="dropdown-menu shadow">
+              <li class="dropend">
+                <a href="#" class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside">Pertemuan 1</a>
+                <ul class="dropdown-menu shadow">
+                  <li><a class="dropdown-item" href="https://temorubun.site/ayub/php/teori/materi1/"> Materi PHP 1</a></li>
+                  <li><a class="dropdown-item" href="https://temorubun.site/ayub/php/teori/tugas1/"> Tugas PHP 1</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" data-bs-auto-close="outside">PRAKTEK PEMOGRAMAN</a>
+            <ul class="dropdown-menu shadow">
+              <li class="dropend">
+                <a href="#" class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside">Pertemuan 1</a>
+                <ul class="dropdown-menu shadow">
+                  <li><a class="dropdown-item" href="https://temorubun.site/ayub/php/praktek/materi1/"> Materi PHP 1</a></li>
+                  <li><a class="dropdown-item" href="https://temorubun.site/ayub/php/praktek/tugas1/"> Tugas PHP 1</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <section class="my-5">
+    <div class="container">
+      <div class="p-4 border">
+
+        <h3>TUGAS PRAKTIKUM PHP 1</h3><br>
+
+        <h4> Berikut ini adalah 5 contoh soal yang berkaitan dengan if statement di PHP:</h4><br>
+
+        <table>
+
+            <tr>
+
+                <td><h4>1.</h4></td>
+                <td><h4>Buatlah program untuk mengecek apakah sebuah bilangan bulat positif atau negatif!</h4></td>
+
+            </tr>
+
+            <tr>
+
+                <td><h4>2.</h4></td>
+                <td><h4>Buatlah program untuk mengecek apakah sebuah bilangan bulat genap atau ganjil!</h4></td>
+
+            </tr>
+
+            <tr>
+
+                <td><h4>3.</h4></td>
+                <td><h4>Buatlah program untuk mengecek apakah seorang siswa lulus atau tidak berdasarkan nilai ujian yang diberikan (nilai kelulusan minimal adalah 60)!</h4></td>
+
+            </tr>
+
+            <tr>
+
+                <td><h4>4.</h4></td>
+                <td><h4>Buatlah program untuk mengecek apakah sebuah kata atau kalimat merupakan palindrom atau tidak (palindrom adalah kata atau kalimat yang jika dibalik tetap sama)!</h4></td>
+
+            </tr>
+
+            <tr>
+
+                <td><h4>5.</h4></td>
+                <td><h4>Buatlah program untuk mengecek apakah sebuah tahun merupakan tahun kabisat atau tidak (tahun kabisat adalah tahun yang habis dibagi 4 dan tidak habis dibagi 100, kecuali tahun yang habis dibagi 400)!</h4></td>
+
+            </tr>
+
+        </table>      
+        <hr>
+      <p>Link Github: <a href="https://github.com/ayub002" target="_blank">https://github.com/Ayub-Maniagasi</a></p>
+    </div>
+    </div>
+  </section>
+
+  <footer class="bg-dark p-4 text-white text-center">
+    <small>CopyRaight © Ayub Maniagasi 2023</small>
+  </footer>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
